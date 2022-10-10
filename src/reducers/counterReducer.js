34c108896fcf5f29a2ac5ad3fe6256266d5f2f89
HirenLimbasiya <107846821+HirenLimbasiya@users.noreply.github.com>
2022-10-10 08:52:04 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,11 @@
 const counterReducer = (state,action) =>{
-
+    if(action.type === "INC"){
+        return state + 1
+    }
+    if(action.type === "DEC"){
+        return state - 1
+    }
+    return state
 }
 
 export {counterReducer}
